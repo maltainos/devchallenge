a76a0164613faf0e1532848f4294489cfff93487
Maltainos <rvwigualzynhoMaltainos@gmail.com>
2022-05-10 13:01:38 +0200

    Commit Entity mapping ORM hibernate JPA

--- src/main/java/com/ibi/challenge/ws/io/entity/Pais.java
+++ src/main/java/com/ibi/challenge/ws/io/entity/Pais.java
@@ -1,5 +1,41 @@
 package com.ibi.challenge.ws.io.entity;
 
-public class Pais {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@Entity
+@Table(name = "paises")
+public class Pais {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false, unique = true, length = 35)
+	private String paisId;
+	
+	@Column(nullable = false, unique = true, length = 125)
+	private String nome;
+	
+	@Column(nullable = false, unique = true, length = 45)
+	private String capital;
+	private float area;
 }
--- src/main/java/com/ibi/challenge/ws/io/entity/Regiao.java
+++ src/main/java/com/ibi/challenge/ws/io/entity/Regiao.java
@@ -1,5 +1,37 @@
 package com.ibi.challenge.ws.io.entity;
 
-public class Regiao {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@Entity
+@Table(name = "regioes")
+public class Regiao {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false, unique = true, length = 35)
+	private String regiaoId;
+	
+	@Column(nullable = false, unique = true, length = 35)
+	private String nome;
 }
